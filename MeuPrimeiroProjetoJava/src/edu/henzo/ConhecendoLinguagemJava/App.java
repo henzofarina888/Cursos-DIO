c1ceda46b79374b2fe689c9e2f3fc0186589d3bf
henzofarina888 <henzofarina8@gmail.com>
2023-02-09 03:16:42 -0300

    variaveis

--- MeuPrimeiroProjetoJava/src/edu/henzo/ConhecendoLinguagemJava/App.java
+++ MeuPrimeiroProjetoJava/src/edu/henzo/ConhecendoLinguagemJava/App.java
@@ -8,9 +8,20 @@ public class App {
 
         String nomeCompletro = nomeCompleto(nome1, nome2);
         System.out.println(nomeCompletro);
-        System.out.println(nomeCompletro);
-        System.out.println(nomeCompletro);
 
+        int num= 10;
+        System.out.println(num);
+        num = -num;
+        System.out.println(num);
+        num= -num;
+        System.out.println(num);
+
+        int a = 1;
+        int b = 1;
+        String resul= a==b ? "Verdadeiro" : "Falso";
+        System.out.println(resul);
+        
+        System.out.println( ++a);
     }
 
     public static String nomeCompleto(String nome1, String nome2) {
