aca5ced838aebfa2c8176f43258c3aede96cd332
henzofarina888 <henzofarina8@gmail.com>
2023-02-09 02:58:35 -0300

    operadores

---
+++ MeuPrimeiroProjetoJava/src/edu/henzo/ConhecendoLinguagemJava/App.java
@@ -0,0 +1,19 @@
+package edu.henzo.ConhecendoLinguagemJava;
+
+public class App {
+    public static void main(String[] args){
+        System.out.println("Bem vindos a meu primeiro projeto java >>>");
+        String nome1 = "Henzo";
+        String nome2 = "Farina";
+
+        String nomeCompletro = nomeCompleto(nome1, nome2);
+        System.out.println(nomeCompletro);
+
+    }
+
+    public static String nomeCompleto(String nome1, String nome2) {
+        return nome1 + " "+ nome2;
+    }
+
+    
+}
